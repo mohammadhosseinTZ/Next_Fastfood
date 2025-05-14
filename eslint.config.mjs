e1841d79de4645cd4c50a32e9c47aad18e250d88
mohammadhossein <146634977+mohammadhosseinTZ@users.noreply.github.com>
2025-05-14 17:47:34 +0330

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -24,10 +24,9 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.config({
     extends: ['next'],
-    settings: {
-      next: {
-        rootDir: 'packages/my-app/',
-      },
+    rules: {
+      'react/no-unescaped-entities': 'off',
+      '@next/next/no-page-custom-font': 'off',
     },
   }),
 ]
