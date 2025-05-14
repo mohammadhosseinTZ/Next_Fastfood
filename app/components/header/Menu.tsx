3fa1f797d1231306fc7ac9fa51cafedfc02d079f
mohammadhossein <hossein.taeezadeh17@gmail.com>
2025-05-14 17:30:39 +0330

    new

--- app/components/header/Menu.tsx
+++ app/components/header/Menu.tsx
@@ -9,8 +9,8 @@ interface NavProps{
 function Menu({ nav, setNav }: NavProps) {
     return (
         <div className={style.menu } onClick={()=>setNav(!nav)}>
-            <Image src={"./images/menu.png"} alt="menu" layout="responsive" width={100} height={100}  className={style.menuIcon}/>
-            <Image src={"./images/close.png"} alt="menu" layout="responsive" width={100} height={100} className={style.closeIcon} />
+            <Image src={"/images/menu.png"} alt="menu" layout="responsive" width={100} height={100}  className={style.menuIcon}/>
+            <Image src={"/images/close.png"} alt="menu" layout="responsive" width={100} height={100} className={style.closeIcon} />
         </div>
     )
 }
--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  reactStrictMode:false ,
-  output : "export"
+  reactStrictMode:false 
 };
 
 export default nextConfig;
